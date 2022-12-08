e3598e8728de324f9b081754313ac54c7a7227ae
STVECLRK <117930280+STVECLRK@users.noreply.github.com>
2022-12-08 09:15:08 -0500

    estep calibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1172,7 +1172,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 137.452 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
